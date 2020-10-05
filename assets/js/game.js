03b610e59ef061af7372088c7b01fb06cc40c32b
tmstafford <tatumstafford1@gmail.com>
2020-10-05 16:33:05 -0500

    while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+
+    //repeat and execute as long as the enemy robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose." );
     console.log(promptFight);
@@ -68,8 +69,12 @@ var fight = function(enemyName) {
 } else {
         window.alert("You need to pick a valid option. Try again!");
 }
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName); 
 }
+
