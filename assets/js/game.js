e62e6c759ea494214ca389dd6b27b6fb1b69e38a
tmstafford <tatumstafford1@gmail.com>
2020-10-05 16:09:56 -0500

    fight multiple enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,21 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//      * Fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -26,7 +32,7 @@ var fight = function() {
 
     //check enemy's health
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert( + " has died!");
     } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
@@ -64,5 +70,6 @@ var fight = function() {
 }
 };
 
-
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
