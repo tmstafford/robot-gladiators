cc79b65c79a2b766e44467227ab8869fef779346
tmstafford <tatumstafford1@gmail.com>
2020-10-06 16:57:00 -0500

    player name prompt error

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,7 +76,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-
+            
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
         
@@ -152,8 +152,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+    
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name");
+    }
+
+    console.log("Your robot's name is "  + name); 
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
